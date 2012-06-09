9b4deabaed56bbec69604b019f98615224744b7f
KanagawaHiroshi <goldriver.1492@gmail.com>
2012-06-09 12:47:41 +0900

    名前変更

--- Assets/Cube.js
+++ Assets/Cube.js
@@ -1,12 +1,14 @@
 ﻿#pragma strict
 
 function Start () {
+	
 	Debug.Log("Hello GitHub!!");
 	Debug.Log("Ryunosuke Funo!!");
 	Debug.Log("Keijiro Takahashi");
 	Debug.Log("KazuyukiIwasa");
 	Debug.Log("Yuya Oda!!");
     Debug.Log("Shingo Tanaka!!");
+    Debug.Log("Hiroshi Kanagawa!!");
 }
 
 function Update () {
