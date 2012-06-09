ae58e2dd9474fa4f67c88b7c1158b0839c08c78c
KazuyukiIwasa <k.iwasa1207@gmail.com>
2012-06-09 12:04:54 +0900

    自分の名前を追加しました

--- Assets/Cube.js
+++ Assets/Cube.js
@@ -1,9 +1,10 @@
-#pragma strict
+﻿#pragma strict
 
 function Start () {
 	Debug.Log("Hello GitHub!!");
 	Debug.Log("Ryunosuke Funo!!");
 	Debug.Log("Keijiro Takahashi");
+	Debug.Log("KazuyukiIwasa");
 }
 
 function Update () {
